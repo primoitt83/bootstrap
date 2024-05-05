5a65b415c4b703c1c224ad72d2655bbd4656896f
primoitt83 <primoitt83@yahoo.com.br>
2024-05-05 15:10:30 -0300

    1st

---
+++ k8s-lubuntu20.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+sudo swapoff -a
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo apt-get update
+#sudo apt-get install -y kubelet kubeadm kubectl
+apt-get install -y kubelet=1.28.2-1.1 kubeadm=1.28.2-1.1 kubectl=1.28.2-1.1 kubernetes-cni=1.2.0-2.1
+
+sudo apt-mark hold kubelet kubeadm kubectl kubernetes-cni
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-mark unhold containerd.io
+
+sudo apt-get update && sudo apt-get install -y containerd.io=1.6.27-1
+
+sudo apt-mark hold containerd.io
+
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+#sudo systemctl status containerd
+
+sudo systemctl enable --now kubelet
+systemctl restart kubelet
---
+++ lubuntu24.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Testado no Lubuntu 18.04
+
+# ssh como root
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+
+sed -i "s/\#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
+
+# desativar ipv6
+cp /etc/default/grub /etc/default/grub.bak
+
+sed -i 's/\GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash ipv6.disable=1"/g' /etc/default/grub
+sed -i 's/\GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/g' /etc/default/grub
+
+update-grub
+
+apt-get install -y ca-certificates curl gnupg lsb-release
+
+mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt install -y containerd.io=1.6.31-1
+apt install -y docker-ce-cli=5:20.10.21~3-0~ubuntu-jammy
+apt install -y docker-ce-rootless-extras=5:20.10.21~3-0~ubuntu-jammy
+apt install -y docker-scan-plugin=0.23.0~ubuntu-jammy
+apt install -y docker-compose-plugin=2.11.2~ubuntu-jammy
+apt install -y docker-ce=5:20.10.21~3-0~ubuntu-jammy
+
+systemctl enable docker
+systemctl start docker
+
+curl -L https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#reboot
